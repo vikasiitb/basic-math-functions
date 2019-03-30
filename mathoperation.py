5b00085f5c57d79ea4469d902b62e9a149bb984c
vikasiitb <49070379+vikasiitb@users.noreply.github.com>
2019-03-30 13:26:07 +0530

    Create mathoperation.py
    Added basic adding, subtracting and multiplication for only 2 numbers.

---
+++ mathoperation.py
@@ -0,0 +1,20 @@
+
+x = int(input("Enter first number"))
+y = int(input("Enter second number"))
+
+a = input("What kind of operation do you wish to perform?\n\
+A) Addition\n\
+B) Subtraction\n\
+C) Multiplication\n\
+D) Remainder\n\
+")
+
+if a in ['A', 'A)','(A)','A) Addition']:
+	z = x + y
+	print(z)
+elif a in ['B','B)','(B)','B) Subtraction']:
+	z = x - y
+	print(z)
+elif a in ['C','C)','(C)','C) Multiplication']:
+	z = x * y
+	print(z)
